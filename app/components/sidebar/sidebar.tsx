6acc04a23ad533f0a07899d7ad6654d7df606db3
gabriel matias <gmsilva1@uniara.edu.br>
2024-03-17 16:29:56 -0300

    iniciando sidebar

---
+++ app/components/sidebar/sidebar.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+function Sidebar () {
+    return 
+        <SidebarStyled>
+            sidebar 
+
+        </SidebarStyled>
+}
+ const SidebarStyled = styled.nav`
+ 
+ 
+ `;
+export default Sidebar;
